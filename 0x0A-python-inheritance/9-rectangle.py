ddb89479b0436cd69f668c471f6a7011f9ae7433
1uiscalderon <1857@holbertonschool.com>
2020-10-07 00:25:17 +0000

    Task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle(BaseGeometry):
     def area(self):
         """Area """
         return self.__width * self.__height
-    
+
     def __str__(self):
         """Print the attibutes"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
