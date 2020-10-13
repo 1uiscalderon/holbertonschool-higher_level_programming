f75319760631a4133d2e56177ae4e3340774ec03
1uiscalderon <1857@holbertonschool.com>
2020-10-13 17:30:29 +0000

    Display error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,9 +72,11 @@ class Rectangle(Base):
 
     def display(self):
         """Print rectangle using #"""
-        print("\n" * self.__x, end="")
+        if self.__y > 0:
+            print("\n" * self.__x, end="")
         for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+            print(" " * self.__x, end="")
+            print("#" * self.__width)
 
     def __str__(self):
         """Returns a string"""
