f9939583ddea23b758e09e7ffe3efaf86e8001f0
1uiscalderon <1857@holbertonschool.com>
2020-10-13 17:28:18 +0000

    Try

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,8 +72,7 @@ class Rectangle(Base):
 
     def display(self):
         """Print rectangle using #"""
-        if self.__y > 0:
-            print("\n" * self.__x, end="")
+        print("\n" * self.__x, end="")
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
