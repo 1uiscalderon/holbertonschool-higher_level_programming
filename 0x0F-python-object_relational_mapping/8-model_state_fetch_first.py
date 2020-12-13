b18fc1b3d4ab6b8ed8633a17018c24aa473cf246
1uiscalderon <1857@holbertonschool.com>
2020-12-13 12:05:14 -0500

    prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
-        first_state = session.query(State).order_by(State.id)
+        first_state = session.query(State).order_by(State.id).first()
         print("{}: {}".format(first_state.id, first_state.name))
     except Exception:
         print("Nothing")
