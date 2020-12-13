9a208612e204bff73a0ca2f34d1de6a7bce572e5
1uiscalderon <1857@holbertonschool.com>
2020-12-13 10:41:22 -0500

    takes in an argument and displays all values in the states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3
 """Write a script that lists all states from the database hbtn_0e_0_usa
 """
 
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}' ORDER\ 
+    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}' ORDER\
                 BY states.id ASC".format(argv[4]))
     states = cur.fetchall()
     for state in states:
