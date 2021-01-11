2d8c8637c3f99f8aedee2ae6538c0ba33d0d021d
1uiscalderon <1857@holbertonschool.com>
2021-01-11 13:27:47 -0500

    Fixed the exception

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter."""
-from requests
+import requests
 from sys import argv
 
 
 if __name__ == "__main__":
     if len(argv) < 2:
-        letter = ""
+        q = ""
     else:
-        letter = argv[1]
+        q = argv[1]
     try:
-        res = post('http://0.0.0.0:5000/search_user', data={'q': letter})
+        res = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
         user = res.json()
+        print(user)
         if not user:
             print('No result')
         else:
