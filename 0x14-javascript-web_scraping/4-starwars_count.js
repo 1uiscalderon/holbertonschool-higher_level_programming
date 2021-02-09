c91ea0a08dfa4d596235ab629136adce6103532d
1uiscalderon <1857@holbertonschool.com>
2021-02-09 12:40:06 -0500

    Counts the number of times a character apears

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let n = 0;
+    for (const film of films) {
+      for (const char of film.characters) {
+        if (char.includes('18')) {
+          n++;
+          break;
+        }
+      }
+    }
+    console.log(n);
+  }
+});
